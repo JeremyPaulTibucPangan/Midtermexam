cf0357b753a001d839fe265d61a02fcdd38cf027
JeremyPaulTibucPangan <158064215+JeremyPaulTibucPangan@users.noreply.github.com>
2024-04-13 21:02:59 +0800

    Update D:\Midterm\src\App.js

--- D:\Midterm\src\App.js
+++ D:\Midterm\src\App.js
@@ -1,1 +1,21 @@
+import React from 'react';
 
+function Pangan({ display }) {
+  return (
+    <div
+      style={{
+        backgroundColor: 'orange',
+        width: '500px',
+        height: '500px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '20px',
+        border: '1px solid black', 
+      }}
+    >
+      Hello, World!
+      {display}
+    </div>
+  );
+}
